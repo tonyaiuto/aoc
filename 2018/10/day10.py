1ae165811a440f0b14ec552f2f45b8e7a6dd40de
Tony Aiuto <aiuto@google.com>
2018-12-10 11:00:46 -0500

    finish day10

--- 2018/10/day10.py
+++ 2018/10/day10.py
@@ -47,6 +47,7 @@ class Grid(object):
     this.max_y = 0
     this.points = []
     this.time = 0
+    this.dirty = True
 
   def Add(this, point):
     this.points.append(point)
@@ -62,9 +63,13 @@ class Grid(object):
       this.max_y = point.y
     this.points[(point.x, point.y)] = point
 
-  def Print(this):
-    coord_map = {}
-    print('=== t:%d' % this.time)
+  def Bounds(this):
+    if not this.dirty:
+      return
+    this.min_x = 0
+    this.min_y = 0
+    this.max_x = 0
+    this.max_y = 0
     for point in this.points:
       if point.x < this.min_x:
         this.min_x = point.x
@@ -74,11 +79,16 @@ class Grid(object):
         this.max_x = point.x
       if point.y > this.max_y:
         this.max_y = point.y
-      coord_map[(point.x, point.y)] = point
 
+  def Print(this):
+    this.Bounds()
+    this.coord_map = {}
+    for point in this.points:
+      this.coord_map[(point.x, point.y)] = point
+    print('=== t:%d' % this.time)
     for y in range(this.min_y, this.max_y+1):
       for x in range(this.min_x, this.max_x+1):
-        p = coord_map.get((x, y))
+        p = this.coord_map.get((x, y))
         if p:
           sys.stdout.write('#')
         else:
@@ -99,6 +109,19 @@ def sample(g):
   g.Print()
 
 
+def part1(g):
+  g.PassTime(10500)
+  while True:
+    g.PassTime(1)
+    g.Bounds()
+    
+    width = g.max_x - g.min_x
+    height = g.max_y - g.min_y
+    print('time:%d, %d x %d' % (g.time, width, height))
+    if width < 1000 and height < 300:
+      g.Print()
+
+
 if __name__ == '__main__':
   verbose = False
   iarg = 1
@@ -107,8 +130,9 @@ if __name__ == '__main__':
     iarg += 1
   with open(sys.argv[iarg]) as inp:
     points = LoadPoints(inp)
-
   g = Grid()
   for p in points:
     g.Add(p)
-  sample(g)
+  # sample(g)
+  part1(g)
+
