7bdb4fa058fb6b3a1ba1e8f8a2b730a059f9e863
tonyaiuto <aiuto@google.com>
2019-12-15 00:09:21 -0500

    checkpoint other need method

--- 2019/14/day14.py
+++ 2019/14/day14.py
@@ -55,7 +55,7 @@ class Node(object):
         continue
       else:
         line = line[1:]
-        yield 'WTF'
+        yield '<UNKNOWN TOKEN: %s>' % c
 
 
 def P(dict):
@@ -68,25 +68,24 @@ class NanoFactory(object):
       with open(path, 'r') as inp:
         reactions = inp.read()
     self.to_get = {}
+    self.nodes = []
     self.trace = False
     self._parse(textwrap.dedent(reactions))
 
   def _parse(self, s):
     for line in s.strip().split('\n'):
       node = Node.fromText(line)
+      self.nodes.append(node)
       # print(node)
       if node.out_chem in self.to_get:
         raise Exception(
             '%s is yielded two ways. Violates premise' % node.out_chem)
       self.to_get[node.out_chem] = node
 
-  def make(self, t_need, t_chem, state=None):
-    if not state:
-      state = defaultdict(int)
-
+  def make(self, t_need, t_chem, state, g_need):
     target_node = self.to_get[t_chem]
     if state[t_chem] >= t_need:
-      state[t_chem] -= t_need
+      # state[t_chem] -= t_need
       return state
 
     # How many times do we have to run the reaction.
@@ -95,15 +94,34 @@ class NanoFactory(object):
 
     for (need, chem) in target_node.inputs:
       need = need * n_times
+      g_need[chem] += need
       if self.trace:
         print('need %5d of %-8.8s: %s' % (need, chem, P(state)))
       if chem == 'ORE':
         state['ORE'] += need
       else:
-        self.make(need-state[chem], chem, state)
+        self.make(need-state[chem], chem, state=state, g_need=g_need)
+        if state[chem] < need:
+          print('WTF?', chem, 'need:', need, str(target_node), P(state))
         state[chem] -= need
     state[t_chem] += target_node.out_quant * n_times
-    return state
+
+  def min_ore(self):
+    state = defaultdict(int)
+    need = defaultdict(int)
+    self.make(1, 'FUEL', state, need)
+    print('min_or: Need %d %d ORE' % (state['ORE'], need['ORE']))
+
+    need_ore = 0
+    for node in self.nodes:
+      for (i_need, i_chem) in node.inputs:
+        if i_chem == 'ORE':
+          # How much do we need of this precursor
+          t_need = need[node.out_chem]
+          n_times = (t_need + node.out_quant - 1) // node.out_quant
+          need_ore += i_need * n_times
+    print('need_ore:', need_ore)
+    return min(need_ore, state['ORE'])
 
 
 def test_part1():
@@ -116,9 +134,8 @@ def test_part1():
       7 A, 1 E => 1 FUEL
       """)
   assert 6 == len(nf.to_get)
-  state = nf.make(1, 'FUEL')
-  print('test_part1: Need %d ORE' % state['ORE'])
-  assert 31 == state['ORE']
+  min_ore = nf.min_ore()
+  assert 31 == min_ore
 
   nf = NanoFactory(reactions="""\
       9 ORE => 2 A
@@ -130,7 +147,9 @@ def test_part1():
       2 AB, 3 BC, 4 CA => 1 FUEL
       """)
   assert 7 == len(nf.to_get)
-  state = nf.make(1, 'FUEL')
+  min_ore = nf.min_ore()
+  assert 165 == min_ore
+
 
   nf = NanoFactory(reactions="""\
       2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
@@ -146,40 +165,40 @@ def test_part1():
       1 VJHF, 6 MNCFX => 4 RFSQX
       176 ORE => 6 VJHF
       """)
-  state = nf.make(1, 'FUEL')
-  print('test_part1: Need %d ORE' % state['ORE'])
-  assert 180697 == state['ORE']
+  min_ore = nf.min_ore()
+  assert 180697 == min_ore
 
   nf = NanoFactory(reactions="""\
       171 ORE => 8 CNZTR
-      7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
       114 ORE => 4 BHXH
-      14 VRPVC => 6 BMBT
-      6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
-      6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
-      15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
-      13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
-      5 BMBT => 4 WPTQ
       189 ORE => 9 KTJDG
-      1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+      121 ORE => 7 VRPVC
       12 VRPVC, 27 CNZTR => 2 XDBXC
       15 KTJDG, 12 BHXH => 5 XCVML
       3 BHXH, 2 VRPVC => 7 MZWV
-      121 ORE => 7 VRPVC
-      7 XCVML => 6 RJRHP
       5 BHXH, 4 VRPVC => 5 LTCX
+      6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+      14 VRPVC => 6 BMBT
+      15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+      1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+      7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+      7 XCVML => 6 RJRHP
+      13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+      5 BMBT => 4 WPTQ
+      6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
       """)
-  nf.trace = True
-  state = nf.make(1, 'FUEL')
-  print('test_part1: Need %d ORE' % state['ORE'])
-  assert 2210736 == state['ORE']
+  # nf.trace = True
+  min_ore = nf.min_ore()
+  assert 2210736 == min_ore
 
 
 def part1():
   nf = NanoFactory(path='input_14.txt')
-  state = nf.make(1, 'FUEL')
-  print('part1: Need %d ORE' % state['ORE'])
-  # assert 180697 == state['ORE']
+  min_ore = nf.min_ore()
+  print('part1: Need %d ORE' % min_ore)
+  #  445745 445745 is too low
+  # need_ore: 445869 still wrong
+  assert 445745 < min_ore
 
 
 if __name__ == '__main__':
