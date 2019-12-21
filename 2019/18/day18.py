638b4b6f4f59def7cb0facdd4044272a7e525306
tonyaiuto <aiuto@google.com>
2019-12-21 12:26:17 -0500

    more tree

--- 2019/18/day18.py
+++ 2019/18/day18.py
@@ -9,29 +9,37 @@ import map
 
 class Path(object):
 
-   def __init__(self, from_where, start):
-     self.from_where = from_where
-     self.dist = 0
-     self.start = start
-     self.visited = {}
-     self.visited[from_where] = 1
-     # distances to things
-     self.locks = {}
-     self.keys = {}
-     self.forks = []
-     # print('Create path: start', self.start, 'visited', self.visited)
+  def __init__(self, from_where, start):
+    self.from_where = from_where
+    self.dist = 0
+    self.start = start
+    self.visited = {}
+    self.visited[from_where] = 1
+    self.visited[start] = 0
 
-   def __str__(self):
-     return '<Path from %s>' % str(self.start)
+    # distances to things
+    self.locks = {}
+    self.keys = {}
+    self.forks = []
+    # print('Create path: start', self.start, 'visited', self.visited)
 
-   def print(self):
-     print('Path from', self.start)
-     print('  locks: ',
-           ', '.join(['%c @ %d' % (k, v) for k, v in self.locks.items()]))
-     print('  keys: ',
-           ', '.join(['%c @ %d' % (k, v) for k, v in self.keys.items()]))
-     print('  forks: ',
-           ', '.join([str(f) for f in self.forks]))
+  def __str__(self):
+    return '<Path from %s>' % str(self.start)
+
+  def print(self, indent=0, forks=True):
+    sp = '   ' * indent
+    print(sp, 'Path from', self.start)
+    print(sp, '  locks: ',
+          ', '.join(['%c @ %d' % (k, v) for k, v in self.locks.items()]))
+    print(sp, '  keys: ',
+          ', '.join(['%c @ %d' % (k, v) for k, v in self.keys.items()]))
+    if self.forks:
+      print(sp, '  forks: ', ', '.join([str(f) for f in self.forks]))
+
+  def print_tree(self, level=0):
+    self.print(indent=level, forks=False)
+    for fork in self.forks:
+      fork.print_tree(level=level+1)
 
 
 class Vault(object):
@@ -39,6 +47,7 @@ class Vault(object):
   def __init__(self, maze):
     self.maze = maze
     self.set_start()
+    self.path_heads = {}
 
   def set_start(self):
     self.start = None
@@ -48,6 +57,7 @@ class Vault(object):
         return
 
   def walk_path(self, path):
+    self.path_heads[path.start] = -1
     pos = path.start
     while True:
       path.visited[pos] = path.dist
@@ -68,9 +78,13 @@ class Vault(object):
         pos = moves[0]
         continue
       # a fork!!
-      for f in moves:
-        child_path = Path(from_where=pos, start=f)
-        child_path.visited[f] = 1
+      for path_start in moves:
+        path.visited[path_start] = path.dist 
+
+      for path_start in moves:
+        child_path = Path(from_where=pos, start=path_start)
+        # Do not duplicate paths
+        child_path.visited.update(self.path_heads)
         path.forks.append(child_path)
         self.walk_path(child_path)
       break
@@ -103,10 +117,17 @@ def part1():
   maze.close_dead_ends()
   maze.print()
 
+  vault = Vault(maze)
+  path = Path(from_where=(-1, -1), start=vault.start)
+  vault.walk_path(path)
+
+  print('========================================')
+  path.print_tree()
+
   # print('part1:', alignment)
   # assert 3428 == alignment
 
 
-
 if __name__ == '__main__':
   test_part1()
+  part1()
