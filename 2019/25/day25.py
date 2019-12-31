2a6af0f52de5f1b401205d406cf64c3d2e8aa9b1
tonyaiuto <tony.aiuto@gmail.com>
2019-12-31 13:36:52 -0500

    Start day 25. mapping is all wrong. It is an advent style graph.

---
+++ 2019/25/day25.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+
+import sys
+import textwrap
+
+from elf_image import ElfImage
+import intcode
+
+
+class Droid(object):
+
+  def __init__(self, mem):
+    self.computer = intcode.IntCode(list(mem))
+    self.quit = False
+    self.visited = {}
+    self.x = 0
+    self.y = 0
+    self.items = {}
+    self.next_item = 'A'
+    self.visited[(0,0)] = '@'
+
+  def send_program(self, prog):
+    assert len(prog) <= 15
+    for cmd in prog:
+      print(cmd)
+      ascii_code = intcode.code_to_ascii(cmd, sep=' ')
+      self.computer.push_input(ascii_code)
+
+  def loop(self):
+    state = ''
+    while True:
+      state += self.computer.run_until_terminator(['\r', '?'])
+      if state.endswith('Command?'):
+        break
+    print(state)
+    self.analyze(state)
+
+    inp = input()
+    if inp.startswith('qu'):
+      self.quit = True
+      return
+    elif inp == 'map':
+      self.printmap()
+      inp = 'inv'
+    command = inp.strip().split(' ')
+    self.do_command(command)
+
+
+  def do_command(self, command):
+    self.last_x = self.x
+    self.last_y = self.y
+    if command[0] == 'north':
+      self.y -= 1
+    elif command[0] == 'south':
+      self.y += 1
+    elif command[0] == 'east':
+      self.x += 1
+    elif command[0] == 'west':
+      self.x -= 1
+    ascii_code = intcode.code_to_ascii(command, sep=' ')
+    self.computer.push_input(ascii_code)
+
+  def play(self):
+    while not self.quit:
+      self.loop()
+
+  def analyze(self, state):
+    getting_desc = False
+    getting_doors = False
+    getting_items = False
+    got_item = False
+    for line in state.split('\n'):
+      if not line:
+        getting_desc = False
+
+      if not line or not line.startswith('- '):
+        getting_doors = False
+        getting_items = False
+
+      if line.startswith('== '):
+        getting_desc = True
+      elif getting_desc:
+        pass  # we printed it
+
+      elif line == 'Doors here lead:':
+        getting_doors = True
+      elif getting_doors and line.startswith('-'):
+        pass  # we printed it
+
+      elif line == 'Items here:':
+        getting_items = True
+      elif getting_items and line.startswith('-'):
+        got_item = True
+        item = line[2:]
+        item_id = self.items.get(item)
+        if not item_id:
+          item_id = self.next_item
+          self.items[item_id] = item
+          print('== Added %s = %s' % (item_id, item))
+          self.next_item = chr(ord(self.next_item) + 1)
+        self.visited[(self.x, self.y)] = item_id
+      elif line.startswith("""You can't go that way"""):
+        self.visited[(self.x, self.y)] = '#'
+        self.x = self.last_x
+        self.y = self.last_y
+      else:
+        if line and not line.startswith('Command'):
+          print('TODO: ', line)
+    if not got_item:
+      self.visited[(self.x, self.y)] = '.'
+
+
+  def printmap(self):
+    for k in sorted(self.items):
+      print(k, self.items[k])
+    print(self.visited)
+    img = ElfImage.fromPoints(self.visited)
+    img.print()
+
+
+
+def part1():
+  mem = intcode.load_intcode('input_25.txt')
+  droid = Droid(list(mem))
+
+  droid.play()
+
+
+if __name__ == '__main__':
+  part1()
