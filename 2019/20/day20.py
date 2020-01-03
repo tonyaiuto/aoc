fa7cfccb149e40f4a1c43c29c27914bc61d91def
tonyaiuto <aiuto@google.com>
2020-01-02 21:15:11 -0500

    checpoint day 20

--- 2019/20/day20.py
+++ 2019/20/day20.py
@@ -1,163 +1,91 @@
 #!/usr/bin/env python3
 
-from collections import namedtuple
-import sys
-import textwrap
+from collections import defaultdict
 
 import map
-# from memoized import memoized
 
 
 class PlutoMaze(object):
 
-  def __init__(self, maze):
-    self.maze = maze
-    self.set_start()
-    self.path_heads = {}
-    self.top = Path(from_where=(-1, -1), start=self.start)
-    self.cur_path = self.top
-    self.cur_dist = 0
+  def __init__(self):
+    self.maze = map.Map(label_width=2, open=['.'], ignore=[' '])
+
+  def load(self, file):
+    self.maze.load(file)
+    self.label_to_pos = defaultdict(list)
+    for pos, label in self.maze.portals.items():
+      self.label_to_pos[label].append(pos)
+    # print(self.label_to_pos)
+    self.jumps = {}
+    for l, pairs in self.label_to_pos.items():
+      if len(pairs) == 2:
+        self.jumps[pairs[0]] = pairs[1]
+        self.jumps[pairs[1]] = pairs[0]
+
+    self.maze.close_dead_ends()
+
+    self.start = self.label_to_pos['AA'][0]
+    self.end = self.label_to_pos['ZZ'][0]
+
+    def change_to_walls(pos, v):
+      if v not in ('#', '.'):
+        self.maze.walls.add(pos)
+
+    self.maze.for_each_cell(change_to_walls)
 
 
-  def set_start(self):
-    self.start = None
-    for pos, c in self.maze.points.items():
-      if c == '@':
-        self.start = pos
-        return
+  def print(self):
+    self.maze.print()
 
-  def walk_path(self, path):
-    self.path_heads[path.start] = -1
-    pos = path.start
+  def find_paths(self):
+ 
+    for dist,more in self.walk_path(self.start, self.end, dict(), 0):
+      print(dist, more)
+
+
+  def walk_path(self, pos, end, visited, dist):
+
     while True:
-      path.visited[pos] = path.dist
-      content = self.maze.cell(pos)
-      if content.isalpha():
-        path.stuff.append(KeyLock(content, path.dist, path))
-        if content.islower():
-          path.keys[content] = path.dist
-        else:
-          path.locks[content] = path.dist
-        print(path.stuff)
-
-      # now move on
-      moves = self.maze.get_moves(pos, path.visited)
-      # print('at', pos, 'moves', moves)
+      if pos == end:
+        yield dist, 'end'
+      visited[pos] = dist
+      moves = self.maze.get_moves(pos, visited)
+      jump = self.jumps.get(pos)
+      if jump:
+        print('JUMP', jump)
+        moves.append(jump)
+      print('at', pos, 'dist', dist, 'moves', moves)
       if not moves:
         break
-      path.dist += 1
+      dist += 1
       if len(moves) == 1:
         pos = moves[0]
         continue
-      # a fork!!
-      for path_start in moves:  # prevent other forks from backtracking
-        path.visited[path_start] = path.dist
-      for path_start in moves:
-        child_path = Path(parent=path, from_where=pos, start=path_start,
-                          base_dist=path.dist)
-        # Do not duplicate paths
-        child_path.visited.update(self.path_heads)
-        path.forks.append(child_path)
-        self.walk_path(child_path)
+      for branch in moves:
+        for n_dist, more in self.walk_path(branch, end, dict(visited), dist):
+          yield n_dist, more
       break
-    # path.print()
-
-  def find_best_action(self):
-    # start from cur_path, cur_dist
-    reachable = self.cur_path.reachable_targets(self.cur_dist, set())
-    print(reachable)
-    best_door = None
-    for content, thing in reachable.items():
-      dist = thing[1]
-      if content.isupper():
-        key = reachable.get(content.lower())
-        if key:
-          if best_door == None or dist < best_door[1]:
-            best_door = (content, dist, key)
-    return best_door
-
-  def move_to(self, keyloc):
-    self.ploc()
-    key_name = keyloc[0]
-    to_path = keyloc[2]
-    route = self.cur_path.route_to(to_path)
-    if to_path in route:
-      print('can move out to', to_path, route)
-      self.traverse_to(to_path, key_name, route)
-    self.ploc()
-
-  def ploc(self):
-    print('now at path', self.cur_path, 'dist', self.cur_dist,
-          'holding', self.holding)
-
-    
-  def traverse_to(self, to_path, key_name, route):
-    # move from current pos to the new place
-    dist = 0
-    while True:
-      print('loop to, route', to_path, route)
-      nxt = route[0]
-      route = route[1:]
-      if nxt == self.cur_path:
-        continue
-      if self.cur_path.parent == nxt:
-        print("Up not implemented")
-        break
-      else:
-        dist_to_path = nxt.base_dist - self.cur_dist
-        dist += dist_to_path
-        self.cur_path = nxt
-        self.cur_dist = 0
-      if to_path == self.cur_path:
-        break
-    # now we are at the right path, move to the key
-    key_dist = to_path.keys[key_name]
-    dist += key_dist
-    self.cur_dist = key_dist
-    self.pick_up(key_name)
-    print('travese_to: moved', dist)
-    return dist
-
-  def pick_up(self, key_name):
-    self.holding.add(key_name)
-    self.cur_path.drop_key(key_name)
-
-
-  def do_it(self, start_path):
-    self.cur_loc = start_path
-    self.holding = set()
-    self.do_round()
-    self.do_round()
-
-
-  def do_round(self):
-    best_action = self.find_best_action()
-    print('best_action', best_action)
-    self.move_to(best_action[2])
+    yield -1, 'dead end'
 
 
 def test_part1():
-  maze = map.Map()
+  maze = PlutoMaze()
   maze.load('sample20_1.txt')
   maze.print()
+  maze.print()
+  maze.find_paths()
 
 
 
 def part1():
-  maze = map.Map()
+  maze = PlutoMaze()
   maze.load('input_20.txt')
   maze.print()
   print()
   print('After dead end removal')
-  maze.close_dead_ends()
   maze.print()
 
-  vault = Vault(maze)
-  path = Path(from_where=(-1, -1), start=vault.start)
-  vault.walk_path(path)
-
   print('========================================')
-  path.print_tree()
 
   # print('part1:', alignment)
   # assert 3428 == alignment
--- 2019/map.py
+++ 2019/map.py
@@ -54,14 +54,23 @@ def neighbors(pos):
 
 class Map(object):
 
-  def __init__(self):
+  def __init__(self, open=None, ignore=None, label_width=0):
+    # core properties
     self.width = 0
     self.height = 0
-    self.trace = False
     self.wall = '#'
-    self.open = ['.', ' ']
+    self.open = open or set(['.'])
+    self.ignore = ignore or set()
+    self.label_width = label_width
+    self.filler = '_'
+    # computed at load
     self.walls = set()
     self.points = {}
+    self.edge_portals = {}
+    self.interior_portals = {}
+    self.portals = {}
+    # transient
+    self.trace = False
 
   def load(self, path):
     with open(path, 'r') as inp:
@@ -73,36 +82,142 @@ class Map(object):
   def cell(self, pos):
     return self.points.get(pos)
 
+  def for_each_cell(self, func):
+    for x in range(self.width):
+      for y in range(self.height):
+        pos = (x, y)
+        func(pos, self.cell(pos))
+
   def load_from_string(self, s):
     self.positions = []
     self.is_occupied = set()
-    self.height = 0
+    self.height = -self.label_width
     self.width = 0
-    for line in s.strip().split('\n'):
-       x = 0
-       for c in line.strip():
-         if c == self.wall:
-           self.walls.add((x, self.height))
-         else:
-           self.points[(x, self.height)] = c
-         x += 1
-       if self.width == 0:
-         self.width = x
-       else:
-         assert self.width == x
-       self.height += 1
+    pre_lines = [None]
+    last1 = None
+    last2 = None
+    last3 = None
+    for line in s.split('\n'):
+      if line == '':
+        continue
+      x = 0
+      if self.height < 0:
+        # pre_lines.append(line[self.label_width:-self.label_width])
+        pre_lines.append(line)
+        self.height += 1
+        continue
+
+      last1 = last2
+      last2 = last3
+      last3 = line
+      if self.label_width > 0 and self.height >= self.label_width:
+        self.extract_labels([last1, last2, last3], self.height)
+      for c in line[self.label_width:-self.label_width]:
+        if c == self.wall:
+          self.walls.add((x, self.height))
+          self.width = max(self.width, x+1)
+        else:
+          if c in self.ignore:
+            pass
+          else:
+            self.points[(x, self.height)] = c
+        x += 1
+      self.height += 1
+
+    # get the top and bottom labels
+    if self.label_width:
+      pre_lines[0] = ' ' * (self.width + self.label_width * 2)
+      self.extract_labels(pre_lines, -1)
+      self.extract_labels(
+          [last2, last3, ' ' * (self.width + self.label_width * 2)],
+          self.height-1)
+      self.height -= self.label_width
+
+    # fix the right edge portals
+    for pos in list(self.portals):
+      if pos[0] == -1:
+        label = self.portals[pos]
+        del self.portals[pos]
+        pos = (self.width, pos[1])
+        self.portals[pos] = label
+        if label in self.edge_portals:
+          self.edge_portals[label] = pos
+        if label in self.interior_portals:
+          self.interior_portals[label] = pos
+
+    print(self.portals)
+
+
+  def extract_labels(self, lines, y):
+    # Note that y is the scan line, which is past height for bottom label
+    # print(lines)
+    for x in range(self.width):
+      label = ''.join(lines[row][x] for row in range(self.label_width+1))
+      # label = ''.join([c if c.isalpha() else ' ' for c in label])
+      label = label.replace('#', ' ')
+      if len(label.replace('.', '').strip()) > 1:
+        # print(x, y, '|%s|' % label, label[:-self.label_width])
+        if label[0] == ' ':
+          self.portals[(x-self.label_width, y+1)] = label[1:].strip()
+        elif label[self.label_width] == ' ':
+          self.portals[(x-self.label_width, y-self.label_width)] = label[0:self.label_width].strip()
+        else:
+           # raise Exception('Bad label <%s> from lines: %s' % (label, lines))
+           pass
+
+    for x in range(self.width + self.label_width):
+      label = lines[-1][x:x+self.label_width+1]
+      if y == 8:
+        print('XXXXX |%s|' % label)
+      if label[0] in self.open and label[1].isalpha() and label[2].isalpha():
+        # .AB 
+        # print('   ==>', label[1:])
+        self.portals[(x-self.label_width, y)] = label[1:]
+      elif label[2] in self.open and label[0].isalpha() and label[1].isalpha():
+        # AB. 
+        self.portals[(x, y)] = label[0:self.label_width]
+
 
   def print(self):
-    print('   ',
+    margin = '  ' + ' ' * self.label_width
+    print(margin + ' ',
           ''.join(['        %2d' % d for d in range(1, self.width // 10 + 1)]))
-    print('  ', ('0123456789' * (self.width // 10 + 1))[0:self.width])
+    print(margin, ('0123456789' * (self.width // 10 + 1))[0:self.width])
+    if self.label_width > 0:
+      line1, line2 = self._prep_labels(0)
+      print('  ', ''.join(line1))
+      print('  ', ''.join(line2))
     for y in range(self.height):
+      #if self.label_width > 0:
+      #  line1, line2 = self._prep_labels(y)
+      annotation = '%2d ' % y
+      if self.label_width > 0:
+        label = self.portals.get((0, y)) or ' '
+        annotation += '%*s' % (self.label_width, label)
       line = [' '] * self.width
       for x in range(self.width):
         pos = (x, y)
         line[x] = self.wall if pos in self.walls else (
-            self.points.get(pos) or '?')
-      print('%2d' % y, ''.join(line))
+            self.points.get(pos) or self.filler)
+      trailer = ''
+      if self.label_width > 0:
+        trailer = self.portals.get((self.width, y)) or ''
+      print(annotation + ''.join(line) + trailer)
+    if self.label_width > 0:
+      line1, line2 = self._prep_labels(self.height-1)
+      print('  ', ''.join(line1))
+      print('  ', ''.join(line2))
+
+  def _prep_labels(self, y):
+    line1 = [' '] * (self.width + self.label_width*2)
+    line2 = [' '] * (self.width + self.label_width*2)
+    for x in range(self.width):
+      label = self.portals.get((x, y))
+      if label:
+        line1[x+self.label_width] = label[0]
+        line2[x+self.label_width] = label[1]
+    return line1, line2
+
 
   def close_dead_ends(self):
     # for _ in range(1):
