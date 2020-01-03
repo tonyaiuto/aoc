f1dbd93c00c3ebfde5cc0aaa0ded8eef3d41ac97
tonyaiuto <aiuto@google.com>
2020-01-02 23:34:38 -0500

    checkpoitn

--- 2019/20/day20.py
+++ 2019/20/day20.py
@@ -46,21 +46,24 @@ class PlutoMaze(object):
 
     self.maze.for_each_cell(change_to_walls)
 
-
   def print(self):
     self.maze.print()
+
+  def is_pos_on_edge(self, pos):
+    x = pos[0]
+    y = pos[1]
+    return (x == 0 or x == self.maze.width-1
+            or y == 0 or y == self.maze.height-1)
 
   def find_min_path(self):
     best_dist = self.maze.width * self.maze.height
     for dist,more in self.walk_path(self.start, self.end, dict(), 0):
-      print(dist, more)
       if more == 'end':
+        print('got to end in', dist)
         best_dist = min(best_dist, dist)
     return best_dist
 
-
   def walk_path(self, pos, end, visited, dist):
-
     while True:
       if pos == end:
         yield dist, 'end'
@@ -97,8 +100,6 @@ def test_part1():
   best_dist = maze.find_min_path()
   assert 58 == best_dist
 
-
-
 def part1():
   maze = PlutoMaze()
   maze.load('input_20.txt')
@@ -114,16 +115,100 @@ def part1():
   assert 632 == best_dist
 
 
+class RecursivePlutoMaze(PlutoMaze):
+
+  def __init__(self):
+    super(RecursivePlutoMaze, self).__init__()
+    # self.maze = map.Map(label_width=2, open=['.'], ignore=[' '])
+    # self.jumps = {}
+    # self.start = None
+    # self.end = None
+
+  def find_min_path(self):
+
+    self.visited = {}
+    self.level = 0
+    self.visited[self.level] = dict()
+
+    best_dist = self.maze.width * self.maze.height
+    for dist,more in self.walk_path(self.start, self.end, self.visited[0], 0):
+      print(dist, more)
+      if more == 'end':
+        best_dist = min(best_dist, dist)
+    return best_dist
+
+  def walk_path(self, pos, end, visited, dist):
+
+    while True:
+      if pos == end and self.level == 0:
+        yield dist, 'end'
+      visited[pos] = dist
+      moves = self.maze.get_moves(pos, visited)
+      jump = self.jumps.get(pos)
+      if jump and jump in visited:
+        jump = None
+      # print('at', pos, 'dist', dist, 'moves', moves)
+      if not moves and not jump:
+        break
+      dist += 1
+      if len(moves) == 1 and not jump:
+        pos = moves[0]
+        continue
+      for branch in moves:
+        for n_dist, more in self.walk_path(branch, end, dict(visited), dist):
+          yield n_dist, more
+
+      if jump:
+        if self.is_pos_on_edge(jump):
+          # outer jump
+          if self.level == 0:
+            return
+          self.level -= 1
+        else:
+          # inner jump
+          self.level += 1
+          if not self.visited[self.level]:
+            self.visited[self.level] = dict()
+            # mark the jumping off point as visited so we do not go deeper
+            self.visited[self.level][pos] = dist
+        for n_dist, more in self.walk_path(
+            jump, end, self.visited[self.level], dist):
+          yield n_dist, more
+      break
+    yield -1, 'dead end'
+
+
+def test_part2():
+  maze = RecursivePlutoMaze()
+  maze.load('sample20_1.txt')
+  maze.print()
+  best_dist = maze.find_min_path()
+  assert 26 == best_dist
+
+  maze = RecursivePlutoMaze()
+  maze.load('sample20_2.txt')
+  maze.print()
+  best_dist = maze.find_min_path()
+  assert -1 == best_dist
+
+  maze = RecursivePlutoMaze()
+  maze.load('sample20_3.txt')
+  maze.print()
+  best_dist = maze.find_min_path()
+  assert -1 == best_dist
+
+
 def part2():
   maze = PlutoMaze()
   maze.load('input_20.txt')
   maze.print()
   print('========================================')
   best_dist = maze.find_min_path()
-  print('part1:', best_dist)
+  print('part2:', best_dist)
   assert 632 == best_dist
 
 
 if __name__ == '__main__':
   test_part1()
   part1()
+  test_part2()
