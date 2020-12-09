19c425ae317d4dd4ea51e4583e5a79865118b6c5
tonyaiuto <aiuto@google.com>
2020-12-09 14:49:25 -0500

    split intcode

--- 2020/8/day8.py
+++ 2020/8/day8.py
@@ -2,6 +2,7 @@
 
 from collections import defaultdict
 
+import intcode
 from tools import reader
 
 
@@ -34,65 +35,11 @@ def main(input):
   print('part2', res)
 
 
-class Ins(object):
-
-  def __init__(self, opcode, arg):
-    self.opcode = opcode
-    self.arg = int(arg)
-
-  def __repr__(self):
-    return '%s %d' % (self.opcode, self.arg)
-
-
-class Proc(object):
-
-  def __init__(self, trace=False):
-    self.mem = []
-    self.pc = 0
-    self.acc = 0
-    self.trace = trace
-
-  def run(self, loop_detect=False):
-    self.pc = 0
-    self.acc = 0
-    n_cycles = 0
-    seen = set()
-    while self.pc != len(self.mem):
-      n_cycles += 1
-      if n_cycles > 1000:
-        # print('====== infinite loop')
-        return 'loop'
-      if loop_detect:
-        if self.pc in seen:
-          print('DONE', self.acc)
-          return
-        seen.add(self.pc)
-      op = self.mem[self.pc]
-      if self.trace:
-        print('pc', self.pc, 'acc', self.acc, op)
-      self.do_op(op)
-    return
-
-
-  def do_op(self, op):
-    if op.opcode == 'nop':
-      self.pc += 1
-      return
-    if op.opcode == 'acc':
-      self.acc += op.arg
-      self.pc += 1
-      return
-    if op.opcode == 'jmp':
-      self.pc += op.arg
-      return
-    print('bad opcode', op)
-
-
 
 class day8(object):
 
   def __init__(self):
-    self.proc = Proc()
+    self.proc = intcode.Intcode()
 
   def load_file(self, file):
     all = reader.FileReader(file).load()
@@ -107,7 +54,7 @@ class day8(object):
   def do_line(self, line):
     if line:
       x = line.split(' ')
-      self.proc.mem.append(Ins(x[0], x[1]))
+      self.proc.mem.append(intcode.Ins(x[0], x[1]))
 
   def part1(self):
     self.proc.run(loop_detect=True)
@@ -117,9 +64,9 @@ class day8(object):
     for i in range(len(self.proc.mem)):
       op = self.proc.mem[i]
       if op.opcode == 'nop':
-        sub = Ins('jmp', op.arg)
+        sub = intcode.Ins('jmp', op.arg)
       elif op.opcode == 'jmp':
-        sub = Ins('nop', op.arg)
+        sub = intcode.Ins('nop', op.arg)
       else:
          continue
       self.proc.mem[i] = sub
