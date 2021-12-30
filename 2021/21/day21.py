c99a9c0da906a8d31d30e024cce6f42369361f0b
tonyaiuto <tony.aiuto@gmail.com>
2021-12-30 09:55:11 -0400

    clean out day21 old dead ends

--- 2021/21/day21.py
+++ 2021/21/day21.py
@@ -29,13 +29,10 @@ class Player(object):
     self.odd = (player % 2) == 1
     self.pos = start
     self.score = 0
-    self.pos_to_scores = {}
-    for i in range(1, 11):
-      self.pos_to_scores[i] = defaultdict(int)
-    self.pos_to_scores[start][0] = 1
+    # for part2
     self.won = 0
-    self.h = {0: (self.pos, 0)}
     self.wins = 0
+    self.h = {0: (self.pos, 0)}
 
   def __str__(self):
     return 'player %d: space %d, score %d' % (self.player, self.pos, self.score)
@@ -57,24 +54,6 @@ class Player(object):
     self.h[hist] = (pos, score)
     return False
 
-  def would_winO(self, rolls, odd):
-    return False
-    roll_i = [int(c) for c in str(rolls)]
-    prev = 0
-    for r in roll_i[0 if odd else 1:-1:2]:
-      prev = prev * 10 + r
-    cur_roll = roll_i[-1]
-    # print("would_win", roll_i, prev, cur_roll)
-    pos, score = self.h[prev]
-
-    pos = (pos - 1 + cur_roll) % 10 + 1
-    score += pos
-    self.h[prev*10+cur_roll] = (pos, score)
-    # print("h", prev*10+cur_roll, '=', pos, score)
-    if score >= 21:
-       return True
-    return False
-
 
 class day21(aoc.aoc):
 
@@ -88,10 +67,6 @@ class day21(aoc.aoc):
         })
     self.trace = True
 
-  def reset(self):
-    # for future use
-    pass
-
   def do_line(self, line):
     # called for each line of input
     pass
@@ -104,7 +79,6 @@ class day21(aoc.aoc):
 
   def part1(self):
     print('===== Start part 1')
-    self.reset()
 
     dice = 0
     nrolls = 0
@@ -139,7 +113,7 @@ class day21(aoc.aoc):
     self.rolls2u = {}
 
     self.rolls2u[0] = (1, 0, 0)
-    for i in range(5):
+    for i in range(25):
       self.playerturn(self.p1)
       # self.pr2u()
       self.pstate(i, self.p1)
@@ -201,112 +175,6 @@ class day21(aoc.aoc):
     print('  tot:', tot, ', 27^%s' % p)
 
 
-  def Xpart2(self):
-    print('===== Start part 2')
-    self.reset()
-
-    p1_wins = 0
-    p2_wins = 0
-    p_p2s(self.p1.pos_to_scores)
-    for i in range(9):
-      print('== player 1 turn')
-      # p_p2s(self.p1.pos_to_scores)
-      # print('==== begin 2')
-      self.turn2(self.p1, self.p2)
-      p_p2s(self.p1.pos_to_scores)
-
-      if self.p1.won > 444356092776315:
-        print('  Gone to far at', self.p1.won)
-        break
-      print('== player 2 turn')
-      self.turn2(self.p2, self.p1)
-      p_p2s(self.p1.pos_to_scores)
-
-    # assert i < 6
-
-    if self.p1.won > self.p2.won:
-      return self.p1.won
-    return self.p2.won
-
-    if p1_wins > p2_wins:
-      return p1_wins
-    return p2_wins
-
-  def split(self, player, times=27):
-    for pos, scores in player.pos_to_scores.items():
-      for score in scores:
-        scores[score] *= times
-
-  def turn2(self, player, other):
-    # position to # of universes at each score
-    p_to_s = {}
-    for i in range(1, 11):
-      p_to_s[i] = defaultdict(int)
-
-    still_playing = set()
-    won_at = defaultdict(int)
-    winnings = 0
-    for roll in range(3, 10):
-      roll_times = roll_probs[roll]
-      nw = 0
-      for o_pos in range(1, 11):
-        n_pos = (o_pos - 1 + roll) % 10 + 1
-
-        # from o_pos I will be at n_pos roll_times (out of 27)
-
-        """
-	If I was on pos N with and old score of S in U universes,
-        and I am moving to position n_pos, roll_times times
-        with a new score of score + n_pos
-        then p2s[n_pos][new score] increases by U * roll_times
-        # for each old position, how many different scores got us there
-        """
-        for score, n_univ in player.pos_to_scores[o_pos].items():
-          # new score for the people at pos o_pos with prev_score score
-          n_score = score + n_pos
-          new_n_univ = n_univ * roll_times
-          if n_score >= 21:
-            player.won += new_n_univ
-            won_at[n_score] += new_n_univ
-            winnings += new_n_univ
-            nw += 1
-          else:
-            p_to_s[n_pos][n_score] += new_n_univ
-      if nw == 0:
-        still_playing.add(roll)
-      if nw > 0:
-        print('   => won, roll:', roll,  '%2d' % nw, ','.join(['%d: %-6d' % (k, v) for k,v in won_at.items()]))
-
-    player.pos_to_scores = p_to_s
-
-    t = 0
-    for p in p_to_s:
-      scores = p_to_s[p]
-      for score, n in scores.items():
-        t += n
-    # assert (t + player.won) in what_pow_27
-
-    sp = sum([roll_probs[roll] for roll in still_playing])
-    print('sp', sp)
-    assert sp <= 27
-    if winnings > 0:
-      assert sp < 27
-
-    # self.split(player, times=sp)
-    if sp > 0:
-      self.split(other, times=sp)
-
-
-def p_p2s(p_to_s):
-  tot = 0
-  for p in sorted(p_to_s):
-    scores = p_to_s[p]
-    print('  scores@%-2d:' % p, ', '.join(['%2d:%6d' % (s, scores[s]) for s in sorted(scores)]))
-    tot += sum(scores.values())
-  p = what_pow_27.get(tot) or '???'
-  print('  tot:', tot, ', 27^%s' % p)
-
-
 def roll(dice):
   # internal is 0-99, return +1 of each
   roll = dice % 100 + (dice + 1) % 100 + (dice + 2) % 100 + 3
@@ -334,11 +202,7 @@ check_roll(99, 103, 2)
 day21.sample_test("""
 4,8
 """, expect1=739785, expect2=444356092776315)
-#                            44999274953475450
-#                            50207674102141821327
-#                            708329200200264
-#                            822385675458
 
 
 if __name__ == '__main__':
-  day21.run_and_check('input.txt', expect1=None, expect2=None)
+  day21.run_and_check('input.txt', expect1=802452, expect2=270005289024391)
