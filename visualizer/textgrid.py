bc97a6b15267aab259f83755dd30b267307c64fd
Tony Aiuto <aiuto@google.com>
2022-01-06 17:11:40 -0500

    update visualizer

--- visualizer/textgrid.py
+++ visualizer/textgrid.py
@@ -2,23 +2,159 @@
 """Receive state of a grid and chart it.
 
 """
+
 import socket
 import sys
+import threading
+import time
+import tkinter
 from typing import List
 
 from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
 
+class VisualizerCore(object):
+
+  def __init__(self, host='localhost', port=8888):
+    self.server = RPCServer(self, host, port)
+
+    self.tk_root = tkinter.Tk()
+    self.canvas = Canvas(self.tk_root)
+
+  def register_functions(self, rpc_server):
+    rpc_server.register_function(lambda x: x, 'ping')
+    rpc_server.register_function(self.quit)
+    rpc_server.register_function(self.dims)
+    rpc_server.register_function(self.demo_arc1)
+    rpc_server.register_function(self.demo_arc2)
+    rpc_server.register_function(self.demo_arc3)
+    rpc_server.register_function(self.grid)
+    rpc_server.register_function(self.draw_cell)
+
+  def serve(self):
+    self.server.start()
+    print('call canvas.start')
+    self.canvas.start()
+    print('call mainloop')
+    self.tk_root.mainloop()
+    print('backfrom mainloop')
+    print('VisualizerCore.serve: done serving')
+
+  def quit(self):
+    print('exiting')
+    self.server.quit()
+    self.tk_root.quit()
+
+  def dims(self, **kwargs):
+    print('DIMS', kwargs)
+    return 'bar'
+
+  def grid(self, height, width, cell_width=7):
+    # print('grid:', height, width, cell_width)
+    if not cell_width:
+      cell_width = 5
+    self.canvas.make_grid(height=height, width=width, cell_width=cell_width)
+
+  def draw_cell(self, x, y, v, **kwargs):
+    print('draw_cell', x, y, v)
+    self.canvas.draw_cell(x, y, v)
+
+  def demo_arc1(self):
+     coord = 10, 10, 300, 300
+     _ = self.canvas.arc(coord, start=0, extent=120, fill="red")
+     _ = self.canvas.arc(coord, start=120, extent=215, fill="yellow")
+
+  def demo_arc2(self):
+     coord = 10, 10, 300, 300
+     _ = self.canvas.arc(coord, start=120, extent=150, fill="blue")
+
+  def demo_arc3(self):
+     coord = 10, 10, 300, 300
+     _ = self.canvas.arc(coord, start=180, extent=200, fill="green")
+
+
+
+class Canvas(threading.Thread):
+
+  def __init__(self, root, width=400, height=400):
+    super(Canvas, self).__init__()
+    self.daemon = False  # cargocult
+    self.root = root
+    self._done = False
+    self.background = 'white'
+    self.foreground = 'black'
+    self.canvas = None
+    self.resize(width, height)
+    self.cells = {}
+
+  def resize(self, width, height):
+    self.width = width
+    self.height = height
+    print('resize canvas to %d x %d' % (self.width, self.height))
+    if self.canvas:
+      self.canvas.configure(bg=self.background, width=self.width, height=self.height)
+    else:
+      self.canvas = tkinter.Canvas(
+          self.root, width=self.width, height=self.height,
+          bg=self.background, borderwidth=0)
+    self.canvas.pack()
+
+  def run(self):
+    self.canvas.pack()
+
+  def pack(self):
+    self.canvas.pack()
+
+  def arc(self, coord, start, extent, fill):
+    _ = self.canvas.create_arc(coord, start=start, extent=extent, fill=fill)
+    self.canvas.pack()
+
+  def make_grid(self, width, height, cell_width):
+    print('canvas.make_grid', width, height)
+    self.grid_height = height
+    self.grid_width = width
+    self.cell_width = cell_width
+    print('call resize', height*cell_width, width*cell_width)
+    self.resize(height=height*cell_width, width=width*cell_width)
+
+  def draw_cell(self, x, y, v):
+    prev = self.cells.get((x,y))
+    if prev:
+       self.canvas.delete(prev)
+    if v == 0:
+      fill = self.background
+    else:
+      fill = self.foreground
+    self.cells[(x, y)] = self.canvas.create_rectangle(
+        x*self.cell_width+1, y*self.cell_width+1,
+        (x+1)*self.cell_width, (y+1)*self.cell_width,
+        fill=fill, outline=fill)
+    # self.pack()
+
+
+class RPCServer(threading.Thread):
+  """RPCServer wraps a SimpleJSONRPCServer in a thread."""
+
+  def __init__(self, visualizer, host='localhost', port=8888):
+    super(RPCServer, self).__init__()
+    self.visualizer = visualizer
+    self.server = SimpleJSONRPCServer((host, port))
+    self.visualizer.register_functions(self.server)
+
+  def run(self):
+    print("Calling serve_forever")
+    self.server.serve_forever()
+    print("Returned from serve_forever")
+
+  def quit(self):
+    print('RPCServer.quit')
+    # kill myself in another thread so that the current RPC can return.
+    threading.Timer(.5, self.server.shutdown).start()
+
+
 
 def main(args: List[str]):
-  server = SimpleJSONRPCServer(('localhost', 8888))
-  server.register_function(lambda x: x, 'ping')
-  server.register_function(dims)
-  server.serve_forever()
-
-
-def dims(**kwargs):
-  print('DIMS', kwargs)
-  return 'bar'
+  visualizer = VisualizerCore('localhost', 8888)
+  visualizer.serve()
 
 
 if __name__ == '__main__':
--- visualizer/try_server.py
+++ visualizer/try_server.py
@@ -4,6 +4,7 @@
 """
 
 import sys
+import time
 from typing import List
 
 import jsonrpclib
@@ -11,10 +12,49 @@ import jsonrpclib
 def main(args: List[str]):
   server = jsonrpclib.Server('http://localhost:8888')
 
+  if len(args) > 1 and args[1].startswith('q'):
+    print('quit =>', server.quit())
+
+  # sanity(server)
+  grid_demo(server)
+
+  # time.sleep(3)
+  # print('quit =>', server.quit())
+
+
+def grid_demo(server):
+  res = server.grid(width=40, height=40, cell_width=10)
+  print('grid =>', res)
+
+  for y in range(0, 40, 2):
+    for x in range(0, 40, 2):
+      time.sleep(.01)
+      res = server.draw_cell(x, y, 1)
+      if y > 5:
+        res = server.draw_cell(x, y-4, 0)
+
+
+
+def sanity(server):
   res = server.ping('is this not echo?')
   print('ping =>', res)
   res = server.dims(x=100, y=42, y_min=-7)
   print('dims =>', res)
+
+
+def arc_demo(server):
+  time.sleep(1)
+  res = server.demo_arc1()
+  print('demo_arc1 =>', res)
+
+  time.sleep(1)
+  res = server.demo_arc2()
+  print('demo_arc2 =>', res)
+
+  time.sleep(1)
+  res = server.demo_arc3()
+  print('demo_arc3 =>', res)
+
 
 if __name__ == '__main__':
   main(sys.argv)
