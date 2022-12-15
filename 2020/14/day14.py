22b6d8fe75dd0b10bec386f13e2c06e6d7339954
tonyaiuto <tony.aiuto@gmail.com>
2022-12-15 00:31:15 -0400

    day14 remrege again

--- 2020/14/day14.py
+++ 2020/14/day14.py
@@ -1,210 +1,182 @@
-"AOC 2020: day 14"
+#!/usr/bin/env python3
+"AOC 2021: day 14"
 
 from collections import defaultdict
-import functools
+import copy
+import heapq
+import itertools
+import jsonrpclib
 import math
+import time
+import tkinter
 
-from tools import reader
+from tools import aoc
+from tools import gridutils
 
+import textgrid
 
-def sample_test(s, expect, expect2=None):
-  puzz = day14()
-  puzz.load_str(s)
-  res = puzz.part1()
-  if expect != res:
-    print('FAIL: expect', expect, 'got', res)
-    assert expect == res
-  if expect2:
-    res = puzz.part2()
-    if expect2 != res:
-      print('FAIL: expect', expect2, 'got', res)
-      assert expect2 == res
-
-def run2(s):
-  puzz = day14()
-  puzz.mode = 2
-  puzz.load_str(s)
-  res = puzz.part2()
-
-
-def main(input, e1=None, e2=None):
-  puzz = day14()
-  puzz.load_file(input)
-  res = puzz.part1()
-  print('part1', res)
-  if e1 and  e1 != res:
-    print('FAIL: expect', e1, 'got', res)
-    assert e1 == res
-
-  puzz = day14()
-  puzz.mode = 2
-  puzz.load_file(input)
-  res = puzz.part2()
-  print('part2', res)
-  if e2 and  e2 != res:
-    print('FAIL: expect', e2, 'got', res)
-    assert e2 == res
-
-
-def mask_val(word, mask0, mask1):
-   return (word & ~mask0) | mask1
-
-
-def proc_mask(mask):
-  m0 = 0
-  m1 = 0
-  mx = 0
-  for c in mask:
-    m0 <<= 1
-    m1 <<= 1
-    mx <<= 1
-    if c == '0':
-      m0 |= 1
-    elif c == '1':
-      m1 |= 1
-    elif c == 'X':
-      mx |= 1
-    else:
-      print('bad mask', mask)
-      assert fail
-  assert (m0 | m1 | mx) & 0xfffffffff == 0xfffffffff
-  assert (m0 & m1) == 0
-  assert (m0 & mx) == 0
-  assert (m1 & mx) == 0
-  return m0, m1, mx
-
-
-class day14(object):
+class Foo(object):
 
   def __init__(self):
-    self.result1 = None
-    self.result2 = None
+    pass
+
+  def __str__(self):
+    return str(self)
+
+class day14(aoc.aoc):
+
+  animate = False
+
+  def __init__(self):
+    super(day14, self).__init__(
+        reader_params={
+            'by_group': False,
+            'skip_first_blank': True,
+            'strip_lines': True,
+            'verbose': False,
+        })
     self.trace = True
-    self.mask = 0
-    self.mem = {}
-    self.mode = 1
+    self.grid = gridutils.Grid(default_cell=' ')
+    self.hose = 500
+    self.min_x = 500
+    # self.grid.set(self.hose, 0, '+')
+    self.endpoints = []
+    if day14.animate:
+      host = '10.0.0.135'
+      host = 'localhost'
+      # self.canvas = jsonrpclib.Server('http://%s:8888' % host)
+      self.tk_root = tkinter.Tk()
+      self.canvas = textgrid.Canvas(self.tk_root)
+    else:
+      self.canvas = None
+
+  @classmethod
+  def set_animate(cls, animate):
+    cls.animate = animate
 
   def reset(self):
+    # for future use
     pass
-
-  def load_file(self, file):
-    all = reader.FileReader(file).load()
-    for x in all:
-      self.do_line(x)
-    self.post_load()
-
-  def load_str(self, s):
-    all = reader.StringReader(s).load()
-    for x in all:
-      self.do_line(x)
-    self.post_load()
-
 
   def do_line(self, line):
-    # mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-    # mem[8] = 11
-    # mem[7] = 101
-    # mem[8] = 0
-    if line.startswith('mask = '):
-      mask = line[7:]
-      self.raw_mask = mask
-      self.mask0, self.mask1, self.maskx = proc_mask(mask)
-      print('mask %s -> %09x, %09x, %09x' % (mask, self.mask0, self.mask1, self.maskx))
-    elif line.startswith('mem['):
-      addr = 0
-      x = line[4:].replace('] = ', ',').split(',')
-      addr = int(x[0])
-      val = int(x[1])
-      print('  mem[%3d] <- %3d %6x %09x' % (addr, val, val, mask_val(val, self.mask0, self.mask1)))
-      if self.mode == 1:
-        self.mem[addr] = mask_val(val, self.mask0, self.mask1)
-      else:
-        self.write_many(addr, val)
-    else:
-      print('bad input', line)
-      assert False
+    # called for each line of input
+    last_x = None
+    lasy_y = None
+    for part in line.split(' '):
+      if part == '->':
+        continue
+      x, y = part.split(',')
+      x = int(x)
+      y = int(y)
+      if x < self.min_x:
+        self.min_x = x
+      self.grid.set(x, y, '#')
+      if last_x:
+        if last_x == x:
+          for i in aoc.visit_range(last_y, y):
+            self.grid.set(x, i, '#')
+        else:
+          for i in aoc.visit_range(last_x, x):
+            self.grid.set(i, y, '#')
+        self.endpoints.append([(last_x, last_y), (x, y)])
+      last_x = x
+      last_y = y
 
   def post_load(self):
-    pass
+    # called after all input is read
+    self.bottom = self.grid.max_y
+    self.setup_anim()
 
+  def setup_anim(self):
+    if not self.canvas:
+      return
+    self.v_xoff = max(0, self.min_x - self.bottom - 10)
+    res = self.canvas.grid(width=500, height=self.bottom+1, cell_width=2)
+    print('grid =>', res)
+    for line in self.endpoints:
+      sx = line[0][0]
+      sy = line[0][1]
+      ex = line[1][0]
+      ey = line[1][1]
+      pound = ord('#')
+      if sx == ex:
+        for i in aoc.visit_range(sy, ey):
+          self.canvas.draw_cell(sx-self.v_xoff, i, 1)
+      else:
+        for i in aoc.visit_range(sx, ex):
+          self.canvas.draw_cell(i-self.v_xoff, sy, 1)
 
   def part1(self):
     print('===== Start part 1')
     self.reset()
-    self.mode = 1
-    sum = 0
-    for loc, val in self.mem.items():
-      sum += val
-    self.result1 = sum
+    self.grid.print(from_x=490)
+    print('max y:', self.bottom)
 
-    print('part1', self.result1)
-    return self.result1
+    for i in range(10000):
+      if not self.drop_sand():
+         return i
+      if i < 5:
+        # self.grid.print(from_x=490)
+        pass
 
-
-  def write_many(self, addr, val):
-
-    # Find the X's and positions in the mask
-    n_floating_bits = 0          # # of floating bits
-    bits = []       # set of bit values of the floating bits
-    keep_mask = 0   # mask to keep the non-floating address bits on
-    force_on_mask = 0
-    bit = 1 << 35
-    for c in self.raw_mask:
-      keep_mask <<= 1
-      force_on_mask <<= 1
-      if c == 'X':
-        n_floating_bits += 1
-        bits.append(bit)
+  def drop_sand(self):
+    x = self.hose
+    y = 0
+    while True:
+      y = self.fall_from(x, y)
+      if y < 0:
+        return False
+      # down left
+      if self.grid.get(x-1, y+1) == ' ':
+        x = x - 1
+        y = y + 1
+      elif self.grid.get(x+1, y+1) == ' ':
+        x = x + 1
+        y = y + 1
       else:
-        keep_mask |= 1
-        if c == '1':
-          force_on_mask |= 1
-      bit >>= 1
+        self.grid.set(x, y, 'o')
+        if self.canvas:
+          self.canvas.draw_cell(x, y, 1, 'pink')
+          # time.sleep(.005)
+        return True
 
-    print('n_floating_bits', n_floating_bits, bits, range, 1 << n_floating_bits)
-    print('   force_on_mask:%9x, keep_mask: %9x' % (force_on_mask, keep_mask))
-
-    fixed_bits = (addr | force_on_mask) & keep_mask
-    print('   fixed_bits      %9x' % fixed_bits)
-    for faddr in range(1 << n_floating_bits):
-     eaddr = fixed_bits
-     f = faddr
-     for bit in bits:
-       if f & 1 != 0:
-         eaddr |= bit
-       f >>= 1
-     # print('    %d -> set @ %9x %d' % (faddr, eaddr, eaddr))
-     self.mem[eaddr] = val
-
+  def fall_from(self, x, y):
+    for y in range(y, self.bottom+1):
+      # print(x, y, self.grid.get(x, y))
+      if self.grid.get(x, y) != ' ':
+        return y - 1
+    return -1
 
   def part2(self):
     print('===== Start part 2')
-    #self.reset()
-    #self.mode = 2
-    sum = 0
-    for loc, val in self.mem.items():
-      sum += val
-    self.result2 = sum
+    self.reset()
 
-    print('part2', self.result2)
-    return self.result2
+    self.bottom += 2
+    print('max y:', self.bottom)
+    for x in range(self.hose - 2 * self.bottom, self.hose + 2 * self.bottom):
+      self.grid.set(x, self.bottom, '#')
+    self.grid.print()
+
+    for i in range(500000):
+      self.drop_sand()
+      if self.grid.get(self.hose, 0) == 'o':
+        if i % 100 == 0:
+          self.grid.print()
+        return i + 1
+
+    return -1
 
 
-sample_test("""
-mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-mem[8] = 11
-mem[7] = 101
-mem[8] = 0
-""", 165)
 
-run2("""
-mask = 000000000000000000000000000000X1001X
-mem[42] = 100
-mask = 00000000000000000000000000000000X0XX
-mem[26] = 1
-""")
+day14.set_animate(True)
 
+day14.sample_test("""
+498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9
+""", expect1=24, expect2=93)
+
+# day14.set_animate(False)
 
 if __name__ == '__main__':
-  main('input.txt', 6317049172545, 3434009980379)
-  pass 
+  day14.run_and_check('input.txt', expect1=755, expect2=None)
+  pass
