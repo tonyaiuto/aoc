a14acf8b089313cb3ddee1ea4323f229b3677a80
tonyaiuto <tony.aiuto@gmail.com>
2022-12-20 20:15:53 -0400

    checkpoint day20 - does not work

--- 2022/20/day20.py
+++ 2022/20/day20.py
@@ -34,6 +34,7 @@ class day20(aoc.aoc):
     self.trace = True
     self.initial = []
     self.at = {}
+    self.values = set()
 
   def reset(self):
     # for future use
@@ -44,7 +45,8 @@ class day20(aoc.aoc):
     v = int(line)
     self.initial.append(v)
     self.at[v] = len(self.initial) - 1
-    pass
+    assert v not in self.values
+    self.values.add(v)
 
   def post_load(self):
     # called after all input is read
@@ -60,10 +62,54 @@ class day20(aoc.aoc):
     return 42
 
   def mix(self, b):
-    new
+    order = self.at
+
+    print('order', order)
     for v in self.initial:
-      pos = self.at(v)
-      n_pos = (pos + v) % self.size
+      if v == 0:
+        continue
+
+      print('order', order)
+      pos = order[v]
+
+      if pos + v == 0:
+        n_pos = self.size - 1
+      else:
+        n_pos = (pos + self.size + v) % self.size
+      print('move', v, 'from', pos, 'to', n_pos)
+
+      # compute range to decrement or increment
+      if pos < n_pos:
+        disp = -1
+        r_low = pos + 1
+        r_high = n_pos - 1
+      else:
+        disp = 1
+        r_low = n_pos
+        r_high = pos
+ 
+      res = {v: n_pos}
+      for ov, opos in order.items():
+        if ov == v:
+          continue
+        if opos >= r_low and opos <= r_high:
+          order[ov] = opos + disp
+
+      order = res
+
+        """
+        #  a x b  => x a b
+        npos = opos
+        if opos > pos:  # b
+          npos -= 1
+        if opos > n_pos:
+          npos += 1
+        print('   ', ov, 'from', opos, 'to', npos)
+        res[ov] = npos
+        """
+
+      plist = sorted([(pos, v) for v, pos in res.items()])
+      print([v[1] for v in plist])
 
   def part2(self):
     print('===== Start part 2')
