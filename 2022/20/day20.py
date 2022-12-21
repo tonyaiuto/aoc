add43adbe2399cb418589a6c34f398339787b14f
tonyaiuto <tony.aiuto@gmail.com>
2022-12-21 00:06:51 -0400

    chkpoint

--- 2022/20/day20.py
+++ 2022/20/day20.py
@@ -8,12 +8,14 @@ import itertools
 from tools import aoc
 
 
+TRACE=1
+
 
 def order_to_list(order):
   v_p = [(v, p)
          for v, positions in order.items()
          for p in positions]
-  print(v_p)
+  # print(v_p)
   return [x[0] for x in sorted(v_p, key=lambda v: v[1])]
 
 
@@ -39,42 +41,47 @@ class day20(aoc.aoc):
     pass
 
   def pos_to_list(self, order):
-    p_v = [(p, self.initial[p]) for p in order]
-    return [x[1] for x in sorted(p_v)]
+    p_v = [(order[i], self.initial[i]) for i in range(len(order))]
+    # print(p_v)
+    return [x[1] for x in sorted(p_v, )]
 
   def do_line(self, line):
     # called for each line of input
     v = int(line)
-    self.pos.append(len(self.initial))
+    pos = len(self.initial)
+    self.pos.append(pos)
+    if v == 0:
+     self.zero = pos
     self.initial.append(v)
-
-    self.at[v].append(len(self.initial) - 1)
+    # self.at[v].append(len(self.initial) - 1)
     if v in self.values:
-      print("Duplicate", v)
-    assert v not in self.values
+      # print("Duplicate", v)
+      pass
+    # assert v not in self.values
     self.values.add(v)
 
   def post_load(self):
     # called after all input is read
-    print(self.initial)
-    print(self.at)
     self.size = len(self.initial)
+    if self.size < 10:
+      print(self.initial)
+      #XXX print(self.at)
 
   def part1(self):
     print('===== Start part 1')
     self.reset()
     b = list(self.initial)
     order = self.mix(b)
-    print('new order', self.pos_to_list(order))
-    # plist = sorted([(pos, v) for v, pos in order.items()])
-    # res = [v[1] for v in plist]
-    print('final', res)
+    mixed = self.pos_to_list(order)
+    print('mixed=', mixed[0:20])
 
-    zero_pos = order[0]
-    print('zero at', zero_pos)
-    a = res[(zero_pos + 1000) % self.size]
-    b = res[(zero_pos + 2000) % self.size]
-    c = res[(zero_pos + 3000) % self.size]
+    zero_pos = order[self.zero]
+    print('zero moves from ', self.zero, 'to', zero_pos)
+    assert mixed[zero_pos] == 0
+
+    a = mixed[(zero_pos + 1000) % self.size]
+    b = mixed[(zero_pos + 2000) % self.size]
+    c = mixed[(zero_pos + 3000) % self.size]
     print('a b c', a, b, c)
     return a + b + c
 
@@ -83,9 +90,8 @@ Then, the grove coordinates can be found by looking at the 1000th, 2000th, and 3
 """
 
   def mix(self, b):
-    order = dict(self.at)
+    # order = dict(self.at)
 
-    print('order', order)
     positions = list(self.pos)
     for thing in range(self.size):
       v = self.initial[thing]
@@ -99,11 +105,11 @@ Then, the grove coordinates can be found by looking at the 1000th, 2000th, and 3
           np += 1
         n_pos = np % self.size
       else:
-        #if pos + v == 0:
-        # we should be at the end
-        n_pos = (pos + v - 1) % self.size
-      print('')
-      print('  move %3d' % v, 'from', pos, 'to', n_pos)
+        np = pos + v
+        if np <= 0:
+          np -= 1
+        # n_pos = (pos + v - 1) % self.size
+        n_pos = np % self.size
 
       # compute range to decrement or increment
       if pos < n_pos:
@@ -112,21 +118,28 @@ Then, the grove coordinates can be found by looking at the 1000th, 2000th, and 3
         r_high = n_pos
       else:
         disp = 1
-        r_low = n_pos + 1
-        r_high = pos
+        r_low = n_pos
+        r_high = pos - 1
+      if TRACE > 0:
+        # print('')
+        print('  move %3d' % v, 'from', pos, 'to', n_pos, ' adjust range:', r_low, r_high, disp)
 
       for pi, opos in enumerate(positions):
         if opos == pos:
           # print('    ', '%3d' % ov, 'from', opos, 'to', opos+disp)
-          print('    self to', n_pos)
+          if TRACE > 1:
+            print('    self to', n_pos)
           positions[pi] = n_pos
-        if opos >= r_low and opos <= r_high:
+        elif opos >= r_low and opos <= r_high:
           positions[pi] = opos + disp
           ov = self.initial[pi]
-          print('    ', '%3d' % ov, 'from', opos, 'to', opos+disp)
+          if TRACE > 2:
+            print('    ', '%3d' % ov, 'from', opos, 'to', opos+disp)
+      if TRACE > 0:
+        # print('   >positions', positions)
+        print('   >', self.pos_to_list(positions))
 
       """
-      # print("  adjust range:", r_low, r_high, disp)
       for ov, opositions in order.items():
         order[ov] = []
         for opos in opositions:
@@ -158,6 +171,8 @@ day20.sample_test("""
 4
 """, expect1=3, expect2=None)
 
+TRACE=0
 
 if __name__ == '__main__':
+  # part1: not -9746, -5555
   day20.run_and_check('input.txt', expect1=None, expect2=None)
