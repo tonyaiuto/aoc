f0138e43d3c1256fb4784aa639c84596d641ebf7
tonyaiuto <tony.aiuto@gmail.com>
2022-12-26 00:29:10 -0400

    got day24

--- 2022/24/day24.py
+++ 2022/24/day24.py
@@ -85,6 +85,16 @@ class day24(aoc.aoc):
 
   def show(self):
     g = gridutils.Grid()
+    print("Minute", self.move)
+    for x in range(self.right+1):
+      if x != 1:
+        g.set(x, 0, '#')
+      if x != self.right-1:
+        g.set(x, self.bottom, '#')
+    for y in range(self.bottom):
+      g.set(0, y, '#')
+      g.set(self.right, y, '#')
+ 
     for b in self.bliz:
       c = g.get(b.x, b.y)
       v = b.dir
@@ -94,6 +104,8 @@ class day24(aoc.aoc):
         else:
           v = '2'
       g.set(b.x, b.y, v)
+    for p in self.places:
+      g.set(p[0], p[1], 'e')
     g.print()
 
   def move_all(self):
@@ -115,7 +127,8 @@ class day24(aoc.aoc):
         b.y += 1
         if b.y == self.bottom:
           b.y = 1
-    occupied.add((b.x, b.y))
+      occupied.add((b.x, b.y))
+    return occupied
 
   def part1(self):
     print('===== Start part 1')
@@ -123,34 +136,90 @@ class day24(aoc.aoc):
 
     self.places[(1, 0)] = 0
 
-    for i in range(5):
-      self.move += 1
-      occupied = self.move_all()
-      np = {}
-      for pos in self.places:
-        x = pos[0]
-        y = pos[1]
-        for off_x, off_y in [(0, -1), (1, 0), (0, 1), (-1, 0)]:
-          x1 = x + off_x
-          y1 = y + off_y
-          if x1 == 0 or y1 == 0:
-            continue
-          if x1 == self.right or y1 == self.right:
-            continue
-          np = (x1, y1)
-          if np in occupied:
-            continue
-          np[(x1, y1)] = self.move
- 
+    # while True:
+    for i in range(290):
+      np = self.do_turn(goal=(self.right-1, self.bottom))
+      if np is None:
+        break
+      print("Changed # states", len(self.places), len(np))
+      self.places = np
       self.show()
+      if (self.right-1, self.bottom-1) in self.places:
+        print("In the good place")
+        #return self.move+1
+    return self.move
 
-    return 42
+  def do_turn(self, goal):
+    self.move += 1
+    occupied = self.move_all()
+    # print(occupied)
+    ret = {}
+    for pos in self.places:
+      x = pos[0]
+      y = pos[1]
+      for off_x, off_y in [(0, 0), (0, -1), (1, 0), (0, 1), (-1, 0)]:
+        x1 = x + off_x
+        y1 = y + off_y
+        if x1 == goal[0] and y1 == goal[1]:
+          print("DONE ", x1, y1, self.move)
+          return None
+
+        if x1 <= 0 or x1 >= self.right:
+          continue
+        if off_x != 0 or off_y != 0:
+          if y1 <= 0 or  y1 >= self.bottom:
+            continue
+        np = (x1, y1)
+        if np in occupied:
+          # print('occupied', np)
+          continue
+        # print(x1, y1)
+        ret[np] = self.move
+    return ret
 
   def part2(self):
     print('===== Start part 2')
     self.reset()
+    self.places[(1, 0)] = 0
+    for i in range(1000):
+      np = self.do_turn(goal=(self.right-1, self.bottom))
+      if np is None:
+        break
+      print("Changed # states", len(self.places), len(np))
+      self.show()
+      self.places = np
+      if (self.right-1, self.bottom-1) in self.places:
+        print("In the good place #1")
 
-    return 42
+    self.places = {
+        (self.right-1, self.bottom): self.move
+    }
+    self.show()
+    for i in range(1000):
+    # while True:
+      np = self.do_turn(goal=(1, 0))
+      if np is None:
+        break
+      print("Changed # states", len(self.places), len(np))
+      self.places = np
+      self.show()
+      if (1, 0) in self.places:
+        print("In the good place #2")
+
+    self.places = {}
+    self.places[(1, 0)] = self.move
+    for i in range(1000):
+    # while True:
+      np = self.do_turn(goal=(self.right-1, self.bottom))
+      if np is None:
+        break
+      print("Changed # states", len(self.places), len(np))
+      self.places = np
+      self.show()
+      if (self.right-1, self.bottom-1) in self.places:
+        print("In the good place #1")
+
+    return self.move
 
 
 day24.sample_test("""
@@ -160,8 +229,9 @@ day24.sample_test("""
 #>v.><>#
 #<^v^^>#
 ######.#
-""", expect1=18, expect2=None)
+""", expect1=18, expect2=54)
 
 
 if __name__ == '__main__':
-  day24.run_and_check('input.txt', expect1=None, expect2=None)
+  # part2, 598 too low
+  day24.run_and_check('input.txt', expect1=274, expect2=839)
