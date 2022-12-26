172584d43dc22d963d052be58626fd9bf1283370
tonyaiuto <tony.aiuto@gmail.com>
2022-12-26 11:58:45 -0400

    day 25 - snafu numbers

--- 2022/25/day25.py
+++ 2022/25/day25.py
@@ -1,24 +1,75 @@
-#!/usr/bin/env python3
+#e!/usr/bin/env python3
 "AOC 2021: day 25"
 
-from collections import defaultdict
-import copy
-import heapq
-import itertools
-import math
+import sys
 
 from tools import aoc
-from tools import gridutils
+
+S_DIGIT_TO_INT = { '=': -2, '-': -1, '0': 0, '1': 1, '2': 2}
+
+def from_snafu(s):
+  ret = 0
+  for c in s:
+    d = S_DIGIT_TO_INT[c]
+    ret = ret * 5 + d
+  return ret
+
+SAMPLES=[
+      (        1, "1"),
+      (        2, "2"),
+      (        3, "1="),
+      (        4, "1-"),
+      (        5, "10"),
+      (        6, "11"),
+      (        7, "12"),
+      (        8, "2="),
+      (        9, "2-"),
+      (       10, "20"),
+      (       15, "1=0"),
+      (       20, "1-0"),
+      (     2022, "1=11-2"),
+      (    12345, "1-0---0"),
+      (314159265, "1121-1110-1=0"),
+      (     4890, "2=-1=0"),
+    ]
+
+def test_from_snafu():
+  for t in SAMPLES:
+    v = from_snafu(t[1])
+    if v != t[0]:
+      print("fail on", t[1], 'got', v, 'expected', t[0])
+      sys.exit(1)
+
+def test_to_snafu():
+  for t in SAMPLES:
+    s = to_snafu(t[0])
+    if s != t[1]:
+      print("fail on", t[0], 'got', s, 'expected', t[1])
+      sys.exit(1)
 
 
-class Foo(object):
+D_2_S = ['0', '1', '2', '=', '-', '0', '1', '2', '=', '-']
+D_2_S = ['0', '1', '2', '=', '-', '0', '1', '2', '=', '-']
 
-  def __init__(self):
-    pass
+def to_snafu(v):
+  ov = v
+  ret = []
+  while v > 0:
+      last_digit = v % 10
+      sd = D_2_S[last_digit]
+      ret.append(sd)
+  
+      if last_digit >= 3:
+        v += 5
+      if last_digit >= 8:
+        v += 5
+      v -= last_digit
+      v = v // 5
+  return ''.join(reversed(ret))
 
-  def __str__(self):
-    return str(self)
 
+test_to_snafu()
+  
 
 
 class day25(aoc.aoc):
@@ -32,38 +83,38 @@ class day25(aoc.aoc):
             'verbose': False,
         })
     self.trace = True
-
-  def reset(self):
-    # for future use
-    pass
+    self.req = []
 
   def do_line(self, line):
     # called for each line of input
-    pass
-
-  def post_load(self):
-    # called after all input is read
-    pass
-
+    self.req.append(from_snafu(line))
 
   def part1(self):
     print('===== Start part 1')
-    self.reset()
-
-    return 42
+    s = sum(self.req)
+    return to_snafu(s)
 
 
   def part2(self):
-    print('===== Start part 2')
-    self.reset()
-
     return 42
 
 
 day25.sample_test("""
-
-""", expect1=None, expect2=None)
+1=-0-2
+12111
+2=0=
+21
+2=01
+111
+20012
+112
+1=-1=
+1-12
+12
+1=
+122
+""", expect1='2=-1=0', expect2=None)
 
 
 if __name__ == '__main__':
-  day25.run_and_check('input.txt', expect1=None, expect2=None)
+  day25.run_and_check('input.txt', expect1="2-0==21--=0==2201==2", expect2=None)
