05f2b662d705d1dbc13563a7f1ea3ea673a33c8a
Tony Aiuto <aiuto@google.com>
2023-12-21 12:16:58 -0500

    this may take some real thought

--- 2023/20/day20.py
+++ 2023/20/day20.py
@@ -39,12 +39,25 @@ class Module(object):
     if not self.outputs_s:
       self.outputs = []
     self.state = False
+    self.in_list = None
 
   def __str__(self):
     return '%s %d -> %s' % (self.name, self.type, self.outputs_s)
 
   def __repr__(self):
     return self.name
+
+  def short_str(self):
+    return '%s:%c' % (self.name, 'H' if self.state else 'L')
+
+  def as_bits(self):
+    if self.type == FF:
+      return '1' if self.state else '0'
+    order = self.in_list if self.in_list else self.inputs
+    return ' %s:%s' % (
+        self.name,
+        ''.join(['1' if o.state else '0' for o in order])
+    )
 
 
 class day20(aoc.aoc):
@@ -59,10 +72,10 @@ class day20(aoc.aoc):
         })
     self.trace = True
     self.modules = {}
+    self.done = False
 
   def reset(self):
-    # for future use
-    pass
+    self.done = False
 
   def do_line(self, line):
     m = Module(line)
@@ -91,9 +104,40 @@ class day20(aoc.aoc):
 
     for m in to_add.values():
       self.modules[m.name] = m
-    self.broadcaster = self.modules['broadcaster']
-    pass
 
+    # special ones
+    self.broadcaster = self.modules['broadcaster']
+    self.rx = self.modules.get('rx')
+
+    # make the printing nicer
+    self.state_order = [
+      self.modules[m] for m in (
+         sorted([m.name for m in self.modules.values() if m.type == FF])
+         + sorted([m.name for m in self.modules.values() if m.type == CONJ]))
+    ]
+
+    for m in self.modules.values():
+      if m.type == CONJ:
+        print('CONJ', m, "===", m.inputs)
+
+    pf = self.modules.get('pf')
+    if pf:
+      self.set_in_list(pf, ['hk', 'qn', 'zr', 'vx', 'lj', 'fl', 'zn', 'vh', 'cb'])
+    xd = self.modules.get('xd')
+    if xd:
+      self.set_in_list(xd, ['qz', 'xv', 'vl',      'gb', 'hd', 'mg', 'vj'])
+
+  def set_in_list(self, module, module_names):
+    print(module.name, [m.name for m in module.inputs])
+    in_list = [self.modules[name] for name in module_names]
+    done = set(in_list)
+    for m in module.inputs:
+      if m not in done:
+        in_list.append(m)
+    in_list.reverse()
+    assert set(in_list) == set(module.inputs)
+    module.in_list = in_list
+    print(module.name, '->', [m.name for m in module.in_list])
 
   def part1(self):
     print('===== Start part 1')
@@ -111,17 +155,47 @@ class day20(aoc.aoc):
     print(self.counts, '=>', ret)
     return ret
 
+  def part2(self):
+    if self.doing_sample:
+      print('===== sample: skipping part 2')
+      return 1
 
-  def do_button(self):
+    print('===== Start part 2')
+    self.reset()
+    self.counts = {'low': 0, 'high': 0}
+
+    ret = 0
+    self.show_state()
+    while not self.done:
+      if ret == 1024 * 16:
+        break
+      ret += 1
+      if ret % 1024 == 0:
+        print("press", ret)
+      if ret % 1024 == 0 or ret < 1024:
+        self.show_state(tag = '%5d' % ret)
+      self.do_button(stop_on=self.rx)
+
+    for m in self.modules.values():
+      if m.type == CONJ:
+        order = m.in_list if m.in_list else m.inputs
+        print('CONJ', m.name, [x.name for x in order])
+    return ret
+
+  def do_button(self, stop_on=None):
     pulses = deque()
     self.send_pulse(None, self.broadcaster, LOW, pulses)
-    while len(pulses) > 0:
+    while len(pulses) > 0 and not self.done:
       # print(pulses)
       what = pulses.popleft()
       from_m = what[0]
       to_m = what[1]
       pulse = what[2]
+      if (stop_on == to_m) and pulse == LOW:
+        self.done = True
+        return True
       self.send_pulse(from_m, to_m, pulse, pulses)
+    return False
 
   def send_pulse(self, from_m, to_module, pulse, pulses):
     self.counts['high' if pulse else 'low'] += 1
@@ -155,13 +229,12 @@ class day20(aoc.aoc):
 
     assert "can not reach" == 'here'
 
-
-  def part2(self):
-    print('===== Start part 2')
-    self.reset()
-
-
-    return 42
+  def show_state(self, tag=''):
+    # print(' '.join([m.short_str() for m in self.state_order]))
+    out = []
+    for m in self.state_order:
+      out.append(m.as_bits())
+    print(tag, ''.join(out)) 
 
 
 day20.sample_test("""
@@ -177,8 +250,8 @@ broadcaster -> a
 %a -> inv, con
 &inv -> b
 %b -> con
-&con -> output
-""", expect1=11687500, expect2=None)
+&con -> rx
+""", expect1=11687500, expect2=1)
 
 
 if __name__ == '__main__':
