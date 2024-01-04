5e87360e67a3dde5d5f095908d24f4a81b4fd722
Tony Aiuto <aiuto@google.com>
2024-01-03 21:50:48 -0500

    checkpoint19

--- 2023/19/day19.py
+++ 2023/19/day19.py
@@ -3,15 +3,22 @@
 
 from collections import defaultdict
 import copy
-import heapq
-import itertools
-import math
 
 from tools import aoc
 from tools import gridutils
 from tools import qparser
 
+
+# Fake workflow names
+MORE = "_MORE_"
+FINAL_A = "_FINAL_"
+
 class Rule(object):
+  
+  REV_COND = {
+      '<': '>=',
+      '>': '<=',
+  }
 
   def __init__(self, s):
     p = s.split(':')
@@ -37,6 +44,16 @@ class Rule(object):
     if verbose:
       print('  ', self, 'got', val, '=>', ok)
     return ok
+
+  def cond_p(self):
+    # Printable clause that makes rule succeed
+    if self.action == 'R':
+      expr = '%s %s %d' % (self.var, Rule.REV_COND[self.cond], self.value)
+      return expr, MORE
+    expr = '%s %c %d' % (self.var, self.cond, self.value)
+    if self.action == 'A':
+      return expr, FINAL_A
+    return expr, self.action
 
 
 class Workflow(object):
@@ -148,10 +165,12 @@ class day19(aoc.aoc):
       print("==== Rating", rating)
       accept = self.part1_eval(rating)
       if accept:
-        print("  ACCEPTING:", rating, "tot +=", rating.total)
+        if self.doing_sample:
+          print("  ACCEPTING:", rating, "tot +=", rating.total)
         ret += rating.total
       else:
-        # print("  REJECTING:", rating)
+        if self.doing_sample:
+          print("  REJECTING:", rating)
         pass
 
     print("part1", ret)
@@ -198,8 +217,90 @@ class day19(aoc.aoc):
 
   def part2(self):
     print('===== Start part 2')
-    workflow = self.workflows['in']
+    self.useless_verify()
 
+    reduced = self.reduce_workflows()
+    original_n_wf = len(self.workflows)
+    self.workflows = {w.name: w for w in reduced}
+    new_n_wf = len(self.workflows)
+    print('==== DONE WITH REDUCE. From', original_n_wf, 'to', new_n_wf)
+    for w_n in sorted(self.workflows):
+      print(self.workflows[w_n])
+
+    workflow = self.workflows['in']
+    clauses = []
+    self.expand(workflow, clauses)
+
+  def reduce_workflows(self):
+    workflows = set(self.workflows.values())
+    while True:
+      reducible = self.find_reducible(workflows)
+      if not reducible:
+        break
+      for remove in  reducible:
+        workflows.remove(remove)
+        for w in workflows:
+          for rule in w.rules:
+            if rule.action == remove.name:
+              print("replace", remove.name, "with %s in" % remove.next, rule)
+              rule.action = remove.next
+          if w.next == remove.name:
+            print("replace fallback", remove.name, "with %s in" % remove.next, rule)
+            w.next = remove.next
+    return workflows
+
+  def find_reducible(self, workflows):
+    reducible = []
+    for workflow in workflows:
+      if workflow.next == 'A':
+        always_true = True
+        for rule in workflow.rules:
+          if rule.action != 'A':
+            always_true = False
+            break
+        if always_true:
+          reducible.append(workflow)
+          print("Can reduce", workflow)
+      if workflow.next == 'R':
+        always_true = True
+        for rule in workflow.rules:
+          if rule.action != 'R':
+            always_true = False
+            break
+        if always_true:
+          reducible.append(workflow)
+          print("Can reduce", workflow)
+    return reducible 
+
+
+  def expand(self, workflow, clauses):
+    if self.doing_sample:
+      print(" . expand workflow", workflow)
+    # hqn{a<2457:bjm,x<2335:qql,s>1648:tbn,tgg}
+    # 
+    orig_clauses = copy.copy(clauses)
+    for rule in workflow.rules:
+      expr, nxt = rule.cond_p()
+      clauses.append(expr)
+      if nxt == MORE:
+        continue
+      if nxt == FINAL_A:
+        print("A:", ' and '.join(clauses))
+      else:
+        next_workflow = self.workflows[nxt]
+        self.expand(next_workflow, copy.copy(clauses))
+
+      if workflow.next == 'A':  # accept          
+        print("A:", ' and '.join(clauses))
+        return
+      if workflow.next == 'R':  # accept          
+        print("REJECT:", ' and '.join(clauses))
+        return
+      next_workflow = self.workflows[workflow.next]
+      self.expand(next_workflow, orig_clauses)
+
+
+  def useless_verify(self):
     # Explore the data a bit
     counts = defaultdict(int)
     for workflow in self.workflows.values():
@@ -214,20 +315,6 @@ class day19(aoc.aoc):
         assert False
     # Each rule is only used once.  Does this matter?
 
-    """
-in{s<1351:px, s>2770:qs, m<1801:hdj, R}
-px{a<2006:qkq, m>2090:A, s<537:gd, x>2440:R, A}
-
-qkq{x<1416:A,crn}
-crn{x>2662:A,R}
-qs{s>3448:A,lnx}
-lnx{m>1548:A,A}
-hdj{m>838:A,pv}
-pv{a>1716:R,A}
-gd{a>3333:R,R}
-    """
-
-    return 42
 
   def bind_w(self, workflow):
     for r in workflow.rules:
@@ -235,23 +322,23 @@ gd{a>3333:R,R}
 
 day19.sample_test("""
 in{s<1351:px,qqz}
+crn{x>2662:A,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+lnx{m>1548:A,A}
+pv{a>1716:R,A}
 px{a<2006:qkq,m>2090:A,rfg}
 qkq{x<1416:A,crn}
-crn{x>2662:A,R}
 qqz{s>2770:qs,m<1801:hdj,R}
 qs{s>3448:A,lnx}
-lnx{m>1548:A,A}
-hdj{m>838:A,pv}
-pv{a>1716:R,A}
 rfg{s<537:gd,x>2440:R,A}
-gd{a>3333:R,R}
 
 {x=787,m=2655,a=1222,s=2876}
 {x=1679,m=44,a=2067,s=496}
 {x=2036,m=264,a=79,s=2244}
 {x=2461,m=1339,a=466,s=291}
 {x=2127,m=1623,a=2188,s=1013}
-""", expect1=19114, expect2=None) # 167409079868000)
+""", expect1=19114, expect2=167409079868000)
 
 
 if __name__ == '__main__':
