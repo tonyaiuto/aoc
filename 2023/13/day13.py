d6a89749bba586986c90c31616a192d895683cc9
Tony Aiuto <aiuto@google.com>
2024-01-10 22:05:30 -0500

    more

--- 2023/13/day13.py
+++ 2023/13/day13.py
@@ -13,13 +13,12 @@ def is_reflect(slice):
       return False
   return True
 
-
 def find_reflection(values):
   # A reflection needs to hit at least one edge
   l = len(values)
   half = (l + 1) // 2
   best_center = -1
-  for lmatch in range(1, half+1):
+  for lmatch in range(half+1, 0, -1):
     # a match has to be at least 1 long and can be up to the full half
     center_left = lmatch - 1         # is 0 on first iteration
     center_right = l - 1 - lmatch    # is l-2 (last element) on first iteration
@@ -32,11 +31,81 @@ def find_reflection(values):
   return best_center
 
 
+def len_reflect(values, pos):
+  lv = len(values)
+  stop = min(pos, lv - pos)
+  for ret in range(stop-1):
+    # print('lv, stop, pos-ret, post+ret', lv, stop, pos-ret, pos+ret+1)
+    if values[pos-ret] != values[pos+ret+1]:
+      return ret-1
+  return stop
+
+def find_reflection_x(values):
+  lv = len(values)
+  best_center = -1
+  best_len = -1
+  for pos in range(0, lv-1):
+    if values[pos] == values[pos+1]:    
+      max_len = min(pos, lv-pos)
+      print("Try at", pos, 'for max', max_len, 'out of', lv)
+      for reflect_len in range(1, max_len):
+        print("cmp", pos-reflect_len, 'to', pos+1+reflect_len)
+        if values[pos-reflect_len] != values[pos+1+reflect_len]:    
+          if reflect_len > best_len:
+            best_len = reflect_len
+  return best_center
+
+
 def best_variation(rows, cols, skip_h, skip_v):
   #  bhr, bvr = best_variation(rows, cols, 
-  bhr =  variation_points(rows, skip=skip_h)
-  bvr =  variation_points(cols, skip=skip_v)
-  return bhr, bvr
+  most = max(max(rows), max(cols)) * 2
+
+  col_dup = list(cols)
+  row_dup = list(rows)
+  h_reflect = -1
+  v_reflect = -1
+  n_reflect = 0
+
+  bit = 1
+  print("== look for new reflects that are not", skip_h, skip_v)
+  for ci, col_v in enumerate(cols):
+    assert bit == (1 << ci)
+    for ri, row_v in enumerate(rows):
+      if row_dup[ri] & bit:
+        row_dup[ri] = row_v & ~bit
+      else:
+        row_dup[ri] = row_v | bit
+      hr = find_reflection(row_dup)
+      row_dup[ri] = row_v
+      # did not move the reflection point
+      if hr > 0 and hr == skip_h:
+        continue
+
+      if col_dup[ci] & bit:
+        col_dup[ci] = col_v & ~bit
+      else:
+        col_dup[ci] = col_v | bit
+      vr = find_reflection(col_dup)
+      col_dup[ci] = col_v
+      if vr > 0 and vr == skip_v:
+        continue
+
+      if hr < 0 and vr < 0:
+        continue
+
+      assert (hr > 0 and hr != skip_h) or (vr > 0 and vr != skip_v)
+      assert hr < 0 or vr < 0
+
+      if hr > 0:
+        print("change", ri, ci, 'gives new h reflect at', hr, 'skiping', skip_h)
+        h_reflect = hr
+      if vr >= 0 and vr != skip_v:
+        print("change", ri, ci, 'gives new v reflect at', vr, 'skiping', skip_v)
+        v_reflect = vr
+
+    bit <<= 1
+
+  return h_reflect, v_reflect
 
 
 def variation_points(vals, skip):
@@ -57,7 +126,7 @@ def variation_points(vals, skip):
       # print(v2, 'bit', bit)
       r = find_reflection(v2)
       v2[vi] = v
-      if r > 0 and r != skip:
+      if r >= 0 and r != skip:
         print('got new reflect at', r, 'skiping', skip)
         if reflect != r:
           reflect = r
@@ -100,7 +169,8 @@ class day13(aoc.aoc):
            rv += 1
       rows.append(rv)
       print('%5d' % rv, row)
-    print(cols) 
+    print('rows', rows) 
+    print('cols', cols) 
     vr = find_reflection(cols)
     if vr > 0:
       self.sum_vr += vr
@@ -111,9 +181,17 @@ class day13(aoc.aoc):
     print('part1 reflection', vr, hr, '=', 100 * hr + vr)
 
     bhr, bvr = best_variation(rows, cols, skip_h=hr, skip_v=vr)
-    self.sum_hr2 += bhr
-    self.sum_vr2 += bvr
-    print('part2 reflections', bhr, bvr)
+    if bhr > 0:
+      assert bvr < 0
+      self.sum_hr2 += bhr
+    if bvr > 0:
+      assert bhr < 0
+      self.sum_vr2 += bvr
+    print('= part2 reflections', bhr, bvr, 'was', hr, vr)
+    if hr < 0:
+      assert bhr > 0 or vr != bvr
+    if vr < 0:
+      assert bvr > 0 or hr != bhr
 
   def part1(self):
     print('===== Start part 1')
@@ -125,7 +203,6 @@ class day13(aoc.aoc):
     print('===== Start part 2')
     ret = self.sum_vr2 + 100 * self.sum_hr2
 
-    
     # if ret <= 33703:
     if ret <= 35515:
       print("TOO LOW")
