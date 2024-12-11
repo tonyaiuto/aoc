beb6acd87a63b39b232bfab5894e811b2e4b6bbe
tonyaiuto <tony.aiuto@gmail.com>
2024-12-11 09:03:26 -0400

    let newday refresh things

--- tmpl/newday.sh
+++ tmpl/newday.sh
@@ -9,17 +9,22 @@ day=$(printf %d $1)
 day2=$(printf %02d $1)
 year=$(date +%Y)
 if [[ -d $day2 ]] ; then
-  echo "$day2 exists"
-  exit 1
+  echo "$day2 exists.  refreshing"
 fi
   
-mkdir $day2
-sed -e "s/@YEAR@/$year/g" -e "s/@N@/$day/g" -e "s/@N2@/${day2}/g" ../tmpl/Makefile.tmpl >$day2/Makefile
-sed -e "s/@YEAR@/$year/g" -e "s/@N@/$day/g" -e "s/@N2@/${day2}/g" ../tmpl/prog.py >$day2/day${day2}.py
+mkdir -p $day2
+if [[ ! -f $day2/Makefile ]] ; then
+  sed -e "s/@YEAR@/$year/g" -e "s/@N@/$day/g" -e "s/@N2@/${day2}/g" ../tmpl/Makefile.tmpl >$day2/Makefile
+fi
+if [[ ! -f $day2/day${day2}.py ]] ; then
+  sed -e "s/@YEAR@/$year/g" -e "s/@N@/$day/g" -e "s/@N2@/${day2}/g" ../tmpl/prog.py >$day2/day${day2}.py
+fi
 
 cd $day2
 ln -s ../../tools
 chmod a+x *.py
-mkdir ../../input/$year/$day2
-touch ../../input/$year/$day2/input.txt
+if [[ ! -d ../../input/$year/$day2 ]] ; then
+  mkdir ../../input/$year/$day2
+  touch ../../input/$year/$day2/input.txt
+fi
 ln -s ../../input/$year/$day2/input.txt
