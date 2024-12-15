aba080044cf0e8e6d09e646d25ee7f2e83d9c20d
Tony Aiuto <aiuto@google.com>
2024-12-15 17:27:15 -0500

    so close, but so far

--- 2024/15/day15.py
+++ 2024/15/day15.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
 "AOC 2023: day 15"
 
-from collections import defaultdict
-import copy
-import heapq
-import itertools
-import math
+import sys
 
 from tools import aoc
 from tools import gridutils
 
 DIRS=gridutils.DIRS4
 
-move_dir = {
+MOVE_DIR = {
     '^': DIRS[0],
     '>': DIRS[1],
     'v': DIRS[2],
     '<': DIRS[3],
+}
+
+REV_DIR = {
+    '^': DIRS[2],
+    'v': DIRS[0],
 }
 
 
@@ -70,7 +71,7 @@ class day15(aoc.aoc):
     return self.lantern_gps()
 
   def do_move(self, pos, move):
-    new_pos = gridutils.add_vector(pos, move_dir[move])
+    new_pos = gridutils.add_vector(pos, MOVE_DIR[move])
     c = self.grid.get_pos(new_pos)
     if c == '#':
       return pos
@@ -81,7 +82,7 @@ class day15(aoc.aoc):
 
     np = new_pos
     while True:
-      np = gridutils.add_vector(np, move_dir[move])
+      np = gridutils.add_vector(np, MOVE_DIR[move])
       c = self.grid.get_pos(np)
       if c == 'O':
         continue
@@ -129,34 +130,38 @@ class day15(aoc.aoc):
     for move in self.moves:
       pos = self.do_move2(pos, move)
       n += 1
-      if n < 5 and self.doing_sample:
-        self.grid.set_pos(pos, '@')
-        self.grid.print()
-        self.grid.set_pos(pos, '.')
+      #if n < 10 and self.doing_sample:
+      #  print("====== Move", n)
+      #  self.grid.set_pos(pos, '@')
+      #  self.grid.print()
+      #  self.grid.set_pos(pos, '.')
     self.grid.print()
     return self.lantern_gps()
 
   def do_move2(self, pos, move):
-    new_pos = gridutils.add_vector(pos, move_dir[move])
+    new_pos = gridutils.add_vector(pos, MOVE_DIR[move])
     c = self.grid.get_pos(new_pos)
     if c == '#':
       return pos
     if c == '.':
       return new_pos
-    assert c in ('[', ']')
+    if c not in ('[', ']'):
+      print("WTF: not [] at", new_pos, c)
+      return 0
 
     # left right are easy. probably can be combined with up
     # down, but doing this is practice
     if move in ('<', '>'):
       np = new_pos
       while True:
-        np = gridutils.add_vector(np, move_dir[move])
+        np = gridutils.add_vector(np, MOVE_DIR[move])
         c = self.grid.get_pos(np)
         if c in ('[', ']'):
           continue
         if c == '#':
           return pos
         assert c == '.'
+
         # Found the gap, move them over
         x = new_pos[0]
         y = new_pos[1]
@@ -175,24 +180,75 @@ class day15(aoc.aoc):
         self.grid.set_pos(new_pos, '.')
         return new_pos
 
-    # find line of O to wall
-    """
+    # find up/down group of [] to wall 
+    to_move = set()
+    to_clear = set()
     heads = set([new_pos])
+    c = self.grid.get_pos(new_pos)
+    if c == '[':
+      heads.add(gridutils.add_vector(new_pos, DIRS[1]))
+    else:
+      heads.add(gridutils.add_vector(new_pos, DIRS[3]))
+    to_move = set(heads)
+    to_clear = set(heads)
     while len(heads) > 0:
+      new_heads = set()
+      last_heads = set(heads)
       for head in heads:
-        np = gridutils.add_vector(head, move_dir[move])
+        np = gridutils.add_vector(head, MOVE_DIR[move])
         c = self.grid.get_pos(np)
-        if c in ('[', ']'):
-          # STACK EM
-          continue
         if c == '#':
           return pos
-        assert c == '.'
+        if c in ('[', ']'):
+          # STACK EM
+          new_heads.add(np)
+          to_move.add(np)
+          if c == '[':
+            move_to_pos = gridutils.add_vector(np, DIRS[1])
+            new_heads.add(move_to_pos)
+            if move_to_pos not in to_move:
+              to_clear.add(move_to_pos)
+            to_move.add(move_to_pos)
+            #clear_pos = gridutils.add_vector(move_to_pos, REV_DIR[move])
+            #if clear_pos not in to_move and move_to_pos not in heads:
+            #  to_clear.add(clear_pos)
+          else:
+            move_to_pos = gridutils.add_vector(np, DIRS[3])
+            new_heads.add(move_to_pos)
+            if move_to_pos not in to_move:
+              to_clear.add(move_to_pos)
+            to_move.add(move_to_pos)
+            #clear_pos = gridutils.add_vector(move_to_pos, REV_DIR[move])
+            #if clear_pos not in to_move and move_to_pos not in heads:
+            #  to_clear.add(clear_pos)
+        else:
+          assert c == '.'
+      if self.doing_sample:
+        print("Heads from ", heads, 'to', new_heads,"moving", to_move, "clearing", to_clear)
+      heads = new_heads
+    if last_heads:
+      if self.doing_sample:
+        print("final heads at", last_heads, 'moving', to_move, 'clearing', to_clear)
+        self.grid.set_pos(pos, '@')
+        self.grid.print()
+        self.grid.set_pos(pos, '.')
+      if move == '^':
+        # Move down from y
+        for cell in sorted(to_move, key=lambda x: x[1], reverse=True):
+          above = (cell[0], cell[1]-1)
+          self.grid.set_pos(above, self.grid.get_pos(cell))
+      else:
+        for cell in sorted(to_move, key=lambda x: x[1]):
+          below = (cell[0], cell[1]+1)
+          self.grid.set_pos(below, self.grid.get_pos(cell))
 
+      print("clearing", to_clear)
+      for cell in to_clear:
+        self.grid.set_pos(cell, '.')
+      self.grid.set_pos(new_pos, '@')
+      self.grid.print()
       self.grid.set_pos(new_pos, '.')
-      self.grid.set_pos(np, 'O')
-      return new_pos
-    """
+
     return new_pos
 
 
@@ -218,7 +274,7 @@ vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
 <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
 ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
 v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
-""", expect1=10092, expect2=None)
+""", expect1=10092, expect2=7)
 
 
 if __name__ == '__main__':
